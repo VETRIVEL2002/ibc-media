6ccaa5724c3678812215361cdd88367f84a7d916
VETRIVEL2002 <115209359+VETRIVEL2002@users.noreply.github.com>
2023-06-16 10:56:05 +0530

    Add files via upload

---
+++ challenge 2/my_code.ts
@@ -0,0 +1,64 @@
+{
+  owner: '5E5LzFGYki9y8ZZCn356gyqQqcSwU7xfR3CMaminapHTM9qm',
+  tokenId: 1,
+  collectionId: 1756,
+  attributes: {
+    '0': {
+      name: { _: 'First name' },
+      value: { _: 'Name: 0' },
+      isArray: false,
+      type: 'string',
+      rawValue: { _: 'Name: 0' },
+      isEnum: false
+    },
+    '1': {
+      name: { _: 'Last name' },
+      value: { _: 'Second name 0' },
+      isArray: false,
+      type: 'string',
+      rawValue: { _: 'Second name 0' },
+      isEnum: false
+    }
+  },
+  image: {
+    url: 'https://ipfs.unique.network/ipfs/Qmap7uz7JKZNovCdLfdDE3p4XA6shghdADS7EsHvLjL6jT/nft_image_0.png',
+    fullUrl: 'https://ipfs.unique.network/ipfs/Qmap7uz7JKZNovCdLfdDE3p4XA6shghdADS7EsHvLjL6jT/nft_image_0.png'
+  },
+  properties: [
+    { key: 'a.0', value: '{"_":"Name: 0"}' },
+    { key: 'a.1', value: '{"_":"Second name 0"}' },
+    {
+      key: 'i.u',
+      value: 'https://ipfs.unique.network/ipfs/Qmap7uz7JKZNovCdLfdDE3p4XA6shghdADS7EsHvLjL6jT/nft_image_0.png'
+    }
+  ],
+  nestingChildTokens: [
+    {
+      owner: '0xf8238ccfff8ed887463fd5e0000006dc00000001',
+      tokenId: 1,
+      collectionId: 1757,
+      attributes: {
+        '0': {
+          name: { _: 'Event name' },
+          value: { _: 'Awesome event!' },
+          isArray: false,
+          type: 'string',
+          rawValue: { _: 'Awesome event!' },
+          isEnum: false
+        }
+      },
+      image: {
+        url: 'https://ipfs.uniquenetwork.dev/ipfs/QmTfW8ao3QztNfFcLqniF1REzSZS8Gvj4Qa2k5e7fyo6WT',
+        fullUrl: 'https://ipfs.uniquenetwork.dev/ipfs/QmTfW8ao3QztNfFcLqniF1REzSZS8Gvj4Qa2k5e7fyo6WT'
+      },
+      nestingParentToken: { collectionId: 1756, tokenId: 1 },
+      properties: [
+        { key: 'a.0', value: '{"_":"Awesome event!"}' },
+        {
+          key: 'i.u',
+          value: 'https://ipfs.uniquenetwork.dev/ipfs/QmTfW8ao3QztNfFcLqniF1REzSZS8Gvj4Qa2k5e7fyo6WT'
+        }
+      ]
+    }
+  ]
+}
